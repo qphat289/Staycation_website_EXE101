1a9f3d0e331be9fce7a5cba1f0a2225022bd57da
NguyenThien <ngonguyenthien25082003@gmail.com>
2025-06-07 03:41:11 +0700

    Done 100% UI source code của workflow đăng phòng

---
+++ static/js/config.js
@@ -0,0 +1,29 @@
+// Configuration file for API keys and other settings
+const CONFIG = {
+    // Google Maps API Key - Updated with user's API key
+    GOOGLE_MAPS_API_KEY: 'AIzaSyDQtHXQcHIaZgKYX7pEWQkntBPeTfu1hqQ',
+    
+    // Default map settings
+    DEFAULT_MAP_CENTER: {
+        lat: 10.7769,  // TP.HCM
+        lng: 106.7009
+    },
+    
+    DEFAULT_MAP_ZOOM: 15,
+    
+    // Map styles
+    MAP_STYLES: [
+        {
+            featureType: "poi",
+            elementType: "labels",
+            stylers: [{ visibility: "off" }]
+        }
+    ]
+};
+
+// Export for use in other files
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+} else {
+    window.CONFIG = CONFIG;
+} 
